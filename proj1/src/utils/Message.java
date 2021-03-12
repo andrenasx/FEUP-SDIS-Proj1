7b414f2ba704f8cffa04d1baa1f08576932f9d91
Gustavo Sena <up201806078@fe.up.pt>
2021-03-12 19:00:11 +0000

    Added parse message method

--- proj1/src/utils/Message.java
+++ proj1/src/utils/Message.java
@@ -1,42 +1,55 @@
 package utils;
 
+import java.net.DatagramPacket;
+import java.util.Arrays;
+
 public class Message {
 
-    private String version;
+
     private String type;
+    private String version;
     private int senderId;
     private String fileId;
     private int chunkNumber;
     private int replicationDegree;
     private byte[] body;
 
-    private String header;
+    private DatagramPacket packet;
 
-
-    public Message(String version, String type, int senderId, String fileId, int chunkNumber, int replicationDegree, byte[] body){
-
-        this.version = version;
-        this.type = type;
-        this.senderId = senderId;
-        this.fileId = fileId;
-        this.chunkNumber = chunkNumber;
-        this.replicationDegree = replicationDegree;
-        this.body = body;
+    public Message(DatagramPacket packet){
+        this.packet=packet;
+        this.unpackMessage();
     }
 
+    private void unpackMessage(){
+        String message = new String(this.packet.getData());
+        message = message.substring(0, Math.min(this.packet.getLength(), message.length()));
+        String[] parts = message.split("\r\n\r\n", 2);
+        String[] header = parts[0].replaceAll("( )+", " ").trim().split(" ");
+        this.type = header[0];
+        this.version = header[1];
+        this.senderId = Integer.parseInt(header[2]);
+        this.fileId = header[3];
+        this.chunkNumber = Integer.parseInt(header[4]);
+        this.replicationDegree = Integer.parseInt(header[5]);
+
+        if (parts.length == 2)
+            this.body = Arrays.copyOfRange(this.packet.getData(), parts[0].length() + 4, this.packet.getLength());
+    }
+
+    public boolean typePutChunk() { return this.type.equals("PUTCHUNK"); }
+
+    public boolean typeStored() { return this.type.equals("STORED"); }
+
+    public boolean typeGetChunk() { return this.type.equals("GETCHUNK"); }
+
+    public boolean typeChunk() { return this.type.equals("CHUNK"); }
+
+    public boolean typeDelete() { return this.type.equals("DELETE"); }
+
+    public boolean typeRemoved() { return this.type.equals("REMOVED"); }
 
 
-    public boolean isPutchunk() { return this.type.equals("PUTCHUNK"); }
-
-    public boolean isStored() { return this.type.equals("STORED"); }
-
-    public boolean isGetChunk() { return this.type.equals("GETCHUNK"); }
-
-    public boolean isChunk() { return this.type.equals("CHUNK"); }
-
-    public boolean isDelete() { return this.type.equals("DELETE"); }
-
-    public boolean isRemoved() { return this.type.equals("REMOVED"); }
 
 
 
@@ -132,4 +145,11 @@ public class Message {
     }
 
 
+    public DatagramPacket getPacket() {
+        return packet;
+    }
+
+    public void setPacket(DatagramPacket packet) {
+        this.packet = packet;
+    }
 }
--- proj1/src/utils/Utils.java
+++ proj1/src/utils/Utils.java
@@ -2,5 +2,5 @@ package utils;
 
 public class Utils {
     public static final int CRLF=0xDA;
-    
+
 }
